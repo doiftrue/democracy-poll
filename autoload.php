28c1528d57c1a861786ed88aa54cfe56fab10c79
Kama <tkama@bk.ru>
2026-07-15 06:28:28 +0500

    Internal class aliases removed

--- autoload.php
+++ autoload.php
@@ -20,10 +20,4 @@ spl_autoload_register( static function( $class ) {
 // For backward compatibility. !After spl_autoload_register()
 class_alias( Poll::class, \DemPoll::class );
 class_alias( Support\Helpers::class, Helpers\Helpers::class );
-class_alias( Support\IP::class, Helpers\IP::class );
 class_alias( Support\Kses::class, Helpers\Kses::class );
-class_alias( Support\Messages::class, Helpers\Messages::class );
-class_alias( System\Activator::class, Utils\Activator::class );
-class_alias( System\Migrator__WP_Polls::class, Utils\Migrator__WP_Polls::class );
-class_alias( System\Plugin_Initor::class, Plugin_Initor::class );
-class_alias( System\Upgrader::class, Utils\Upgrader::class );
